dc49dcc2ffb49b14c79c78299288f9a9a7e4a28e
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2025-02-10 00:16:10 -0600

    implementation

--- classes/plant.py
+++ classes/plant.py
@@ -1,4 +1,32 @@
 class Plant:
 
 # Write your plant class here!
-    pass
+    def __init__(self, name, water_level, sunlight_hours, is_blooming=False):
+        self.name = name
+        self.water_level = water_level
+        self.sunlight_hours = sunlight_hours
+        self.is_blooming = is_blooming
+
+    def water(self, water_amount):
+        self.water_level  += water_amount
+
+        return f"{self.name} New water level: {self.water_level}"
+
+    def give_sunlight(self, hours):
+        self.sunlight_hours += hours
+
+    def check_growth(self):
+        if self.sunlight_hours > 5 and self.water_level > 5:
+            self.growth = "mature"
+        else:
+            self.growth = "sprout"
+            
+        # return f"{self.name} is {self.growth_state}"
+
+plant = Plant("Butterfly pea flower", 4, 5, True)
+
+print(plant)
+print(plant.name, plant.water_level, plant.sunlight_hours, plant.is_blooming)
+print(plant.water(3))
+print(plant.check_growth())
+print(plant.growth)
--- tests/test_plant.py
+++ tests/test_plant.py
@@ -1,1 +1,14 @@
 from classes.plant import Plant
+
+def test_plant_initialization():
+    name = "Butterfly pea flower"
+    water_level = 6
+    sunlight_hours = 7
+    is_blooming = True
+    plant = Plant(name, water_level, sunlight_hours, is_blooming)
+    plant_2 = Plant(name, water_level, sunlight_hours)
+    assert plant.name == name
+    assert plant.water_level == water_level
+    assert plant.sunlight_hours == sunlight_hours
+    assert plant.is_blooming == is_blooming
+    assert not plant_2.is_blooming
